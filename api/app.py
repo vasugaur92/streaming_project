dd1f1d63c707e3fe0892b075476cd6f1c5b58ea1
Vasu Gaur <vasu04111@gmail.com>
2025-11-23 23:39:33 +0100

    updated docker config to run locally, consumer and producer code also working

--- api/app.py
+++ api/app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-# from kafka import KafkaProducer
+from kafka import KafkaProducer, KafkaConsumer
 from faker import Faker
+import json
 
 app = FastAPI()
 
@@ -10,13 +11,37 @@ fake = Faker()
 
 @app.get("/generate_event")
 def generate_event():
-    # producer = KafkaProducer(bootstrap_servers=["host.docker.internal:9092"])
+    producer = KafkaProducer(
+        bootstrap_servers=["localhost:9092"],
+        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+        acks="all",
+    )
     event_json = {
         "name": fake.name(),
         "location": fake.city(),
         "customer_id": fake.random_number(digits=5),
         "order_id": fake.random_number(digits=7),
         "amount": fake.random_number(digits=3),
+        "timestamp": fake.iso8601(),
     }
+    producer.send("events_topic", event_json)
+    producer.flush()
 
     return event_json
+
+
+@app.get("/read_event")
+def read_event():
+    consumer = KafkaConsumer(
+        "events_topic",
+        bootstrap_servers=["localhost:9092"],
+        auto_offset_reset="earliest",
+        enable_auto_commit=True,
+        group_id="my-group",
+        value_deserializer=lambda x: json.loads(x.decode("utf-8")),
+        consumer_timeout_ms=1000,
+    )
+    messages = []
+    for message in consumer:
+        messages.append(message.value)
+    return {"messages": messages}
